915619f947ba167bb6e37c061d7e3dd89fb4ad46
YashSejani <yashsejani2007@gmail.com>
2025-05-11 20:16:20 +0530

    add functionality of * / %

--- bigintlibrary.cpp
+++ bigintlibrary.cpp
@@ -3,46 +3,62 @@
 #include <string>
 using namespace std;
 
-vector<int> string_vector(string num) {
+vector<int> string_vector(string num)
+{
     vector<int> result;
-    for (int i = num.size() - 1; i >= 0; --i) {
+    for (int i = num.size() - 1; i >= 0; --i)
+    {
         result.push_back(num[i] - '0');
     }
     return result;
 }
 
-string vector_string(vector<int> numVec) {
+string vector_string(vector<int> numVec)
+{
     string result;
     bool leadingZero = true;
-    for (int i = numVec.size() - 1; i >= 0; --i) {
-        if (leadingZero && numVec[i] == 0) continue;
+    for (int i = numVec.size() - 1; i >= 0; --i)
+    {
+        if (leadingZero && numVec[i] == 0)
+            continue;
         leadingZero = false;
         result += (numVec[i] + '0');
     }
     return result.empty() ? "0" : result;
 }
 
-int compare(string num1, string num2) {
-    if (num1.length() > num2.length()) return 1;
-    if (num1.length() < num2.length()) return -1;
-    for (size_t i = 0; i < num1.length(); ++i) {
-        if (num1[i] > num2[i]) return 1;
-        if (num1[i] < num2[i]) return -1;
+int compare(string num1, string num2)
+{
+    if (num1.length() > num2.length())
+        return 1;
+    if (num1.length() < num2.length())
+        return -1;
+    for (size_t i = 0; i < num1.length(); ++i)
+    {
+        if (num1[i] > num2[i])
+            return 1;
+        if (num1[i] < num2[i])
+            return -1;
     }
     return 0;
 }
 
-vector<int> subtract(vector<int> a, vector<int> b) {
+vector<int> subtract(vector<int> a, vector<int> b)
+{
     vector<int> result;
     int borrow = 0;
-    for (int i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); ++i)
+    {
         int digitA = a[i];
         int digitB = (i < b.size()) ? b[i] : 0;
         int diff = digitA - digitB - borrow;
-        if (diff < 0) {
+        if (diff < 0)
+        {
             diff += 10;
             borrow = 1;
-        } else {
+        }
+        else
+        {
             borrow = 0;
         }
         result.push_back(diff);
@@ -52,50 +68,109 @@ vector<int> subtract(vector<int> a, vector<int> b) {
     return result;
 }
 
-vector<int> add(vector<int> a, vector<int> b) {
+vector<int> add(vector<int> a, vector<int> b)
+{
     vector<int> result;
     int carry = 0;
     int maxLength = max(a.size(), b.size());
 
-    for (int i = 0; i < maxLength; ++i) {
+    for (int i = 0; i < maxLength; ++i)
+    {
         int digitA = (i < a.size()) ? a[i] : 0;
         int digitB = (i < b.size()) ? b[i] : 0;
         int sum = digitA + digitB + carry;
         result.push_back(sum % 10);
         carry = sum / 10;
     }
-    if (carry) result.push_back(carry);
+    if (carry)
+        result.push_back(carry);
     return result;
 }
 
-string subwithsign(string num1, string num2) {
+string subwithsign(string num1, string num2)
+{
     int cmp = compare(num1, num2);
     vector<int> vec1 = string_vector(num1);
     vector<int> vec2 = string_vector(num2);
     vector<int> result;
 
-    if (cmp == 0) {
+    if (cmp == 0)
+    {
         return "0";
-    } else if (cmp > 0) {
+    }
+    else if (cmp > 0)
+    {
         result = subtract(vec1, vec2);
         return vector_string(result);
-    } else {
+    }
+    else
+    {
         result = subtract(vec2, vec1);
         return "-" + vector_string(result);
     }
 }
 
-struct bigint {
+vector<int> multiply(const vector<int> &a, const vector<int> &b)
+{
+    vector<int> result(a.size() + b.size(), 0);
+    for (int i = 0; i < a.size(); ++i)
+    {
+        for (int j = 0; j < b.size(); ++j)
+        {
+            result[i + j] += a[i] * b[j];
+            if (result[i + j] >= 10)
+            {
+                result[i + j + 1] += result[i + j] / 10;
+                result[i + j] %= 10;
+            }
+        }
+    }
+    while (result.size() > 1 && result.back() == 0)
+        result.pop_back();
+    return result;
+}
+
+pair<string, string> divide_mod(const string &dividend, const string &divisor)
+{
+    if (compare(divisor, "0") == 0)
+        return {"NaN", "NaN"};
+    string quotient = "", remainder = "";
+    for (char digit : dividend)
+    {
+        remainder += digit;
+        int count = 0;
+        while (compare(remainder, divisor) >= 0)
+        {
+            remainder = subwithsign(remainder, divisor);
+            if (remainder[0] == '-')
+                remainder = remainder.substr(1);
+            count++;
+        }
+        quotient += (count + '0');
+    }
+    int i = 0;
+    while (i < quotient.size() - 1 && quotient[i] == '0')
+        i++;
+    quotient = quotient.substr(i);
+    return {quotient, remainder};
+}
+
+struct bigint
+{
     bool isNegative;
     string value;
 };
 
-bigint setbigint(const string& str) {
+bigint setbigint(const string &str)
+{
     bigint result;
-    if (str[0] == '-') {
+    if (str[0] == '-')
+    {
         result.isNegative = true;
         result.value = str.substr(1);
-    } else {
+    }
+    else
+    {
         result.isNegative = false;
         result.value = str;
     }
@@ -103,22 +178,31 @@ bigint setbigint(const string& str) {
 }
 
 // Add or subtract depending on sign
-string addorsub(const string& num1, const string& num2) {
+string addorsub(const string &num1, const string &num2)
+{
     bigint a = setbigint(num1);
     bigint b = setbigint(num2);
 
-    if (!a.isNegative && !b.isNegative) {
+    if (!a.isNegative && !b.isNegative)
+    {
         return vector_string(add(string_vector(a.value), string_vector(b.value)));
-    } else if (a.isNegative && b.isNegative) {
+    }
+    else if (a.isNegative && b.isNegative)
+    {
         return "-" + vector_string(add(string_vector(a.value), string_vector(b.value)));
-    } else if (a.isNegative && !b.isNegative) {
+    }
+    else if (a.isNegative && !b.isNegative)
+    {
         return subwithsign(b.value, a.value);
-    } else {
+    }
+    else
+    {
         return subwithsign(a.value, b.value);
     }
 }
 
-int main() {
+int main()
+{
     string num1, num2;
     char op;
 
@@ -131,27 +215,50 @@ int main() {
 
     string result;
 
-    if (op == '+') {
+    if (op == '+')
+    {
         result = addorsub(num1, num2);
-    } else if (op == '-') {
-        if (num2[0] == '-') num2 = num2.substr(1);
-        else num2 = "-" + num2;
+    }
+    else if (op == '-')
+    {
+        if (num2[0] == '-')
+            num2 = num2.substr(1);
+        else
+            num2 = "-" + num2;
         result = addorsub(num1, num2);
-    } else if (op == '*')
-    {
     }
-    else if (op == '/')
+    else if (op == '*')
     {
+        bigint a = setbigint(num1);
+        bigint b = setbigint(num2);
+        string product = vector_string(multiply(string_vector(a.value), string_vector(b.value)));
+        if (a.isNegative != b.isNegative && product != "0")
+            product = "-" + product;
+        result = product;
     }
-    else if (op == '%')
+    else if (op == '/' || op == '%')
     {
+        bigint a = setbigint(num1);
+        bigint b = setbigint(num2);
+        pair<string, string> divmod = divide_mod(a.value, b.value);
+        if (op == '/')
+        {
+            if (a.isNegative != b.isNegative && divmod.first != "0")
+                divmod.first = "-" + divmod.first;
+            result = divmod.first;
+        }
+        else
+        {
+            if (a.isNegative && divmod.second != "0")
+                divmod.second = "-" + divmod.second;
+            result = divmod.second;
+        }
     }
     else
     {
         cout << "Invalid operator!" << endl;
     }
 
-    cout << "Result: " << result << endl;
+    cout << result << endl;
     return 0;
 }
-
